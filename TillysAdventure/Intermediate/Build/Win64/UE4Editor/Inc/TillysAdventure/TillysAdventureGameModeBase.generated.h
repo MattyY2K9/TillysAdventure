f0dc6a9acaec0c31ffe03e0f8f485a09ddb8e656
MattyY2K9 <matthewhardwick30@hotmail.com>
2018-04-14 21:22:33 +0100

    Initial Commit

---
+++ TillysAdventure/Intermediate/Build/Win64/UE4Editor/Inc/TillysAdventure/TillysAdventureGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef TILLYSADVENTURE_TillysAdventureGameModeBase_generated_h
+#error "TillysAdventureGameModeBase.generated.h already included, missing '#pragma once' in TillysAdventureGameModeBase.h"
+#endif
+#define TILLYSADVENTURE_TillysAdventureGameModeBase_generated_h
+
+#define TillysAdventure_Source_TillysAdventure_TillysAdventureGameModeBase_h_15_RPC_WRAPPERS
+#define TillysAdventure_Source_TillysAdventure_TillysAdventureGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define TillysAdventure_Source_TillysAdventure_TillysAdventureGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesATillysAdventureGameModeBase(); \
+	friend TILLYSADVENTURE_API class UClass* Z_Construct_UClass_ATillysAdventureGameModeBase(); \
+public: \
+	DECLARE_CLASS(ATillysAdventureGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/TillysAdventure"), NO_API) \
+	DECLARE_SERIALIZER(ATillysAdventureGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define TillysAdventure_Source_TillysAdventure_TillysAdventureGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesATillysAdventureGameModeBase(); \
+	friend TILLYSADVENTURE_API class UClass* Z_Construct_UClass_ATillysAdventureGameModeBase(); \
+public: \
+	DECLARE_CLASS(ATillysAdventureGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/TillysAdventure"), NO_API) \
+	DECLARE_SERIALIZER(ATillysAdventureGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define TillysAdventure_Source_TillysAdventure_TillysAdventureGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATillysAdventureGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATillysAdventureGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATillysAdventureGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATillysAdventureGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATillysAdventureGameModeBase(ATillysAdventureGameModeBase&&); \
+	NO_API ATillysAdventureGameModeBase(const ATillysAdventureGameModeBase&); \
+public:
+
+
+#define TillysAdventure_Source_TillysAdventure_TillysAdventureGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATillysAdventureGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATillysAdventureGameModeBase(ATillysAdventureGameModeBase&&); \
+	NO_API ATillysAdventureGameModeBase(const ATillysAdventureGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATillysAdventureGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATillysAdventureGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATillysAdventureGameModeBase)
+
+
+#define TillysAdventure_Source_TillysAdventure_TillysAdventureGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define TillysAdventure_Source_TillysAdventure_TillysAdventureGameModeBase_h_12_PROLOG
+#define TillysAdventure_Source_TillysAdventure_TillysAdventureGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TillysAdventure_Source_TillysAdventure_TillysAdventureGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	TillysAdventure_Source_TillysAdventure_TillysAdventureGameModeBase_h_15_RPC_WRAPPERS \
+	TillysAdventure_Source_TillysAdventure_TillysAdventureGameModeBase_h_15_INCLASS \
+	TillysAdventure_Source_TillysAdventure_TillysAdventureGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define TillysAdventure_Source_TillysAdventure_TillysAdventureGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TillysAdventure_Source_TillysAdventure_TillysAdventureGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	TillysAdventure_Source_TillysAdventure_TillysAdventureGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	TillysAdventure_Source_TillysAdventure_TillysAdventureGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	TillysAdventure_Source_TillysAdventure_TillysAdventureGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID TillysAdventure_Source_TillysAdventure_TillysAdventureGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ TillysAdventure/Source/TillysAdventure/TillysAdventureGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TillysAdventureGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TILLYSADVENTURE_API ATillysAdventureGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
